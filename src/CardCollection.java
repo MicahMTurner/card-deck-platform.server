5568d9bda3b0ae585f07d52b8f9512b87d1ab0b1
Irzh <Irzh@localhost>
2012-03-03 20:54:21 +0200

    (no commit message)

---
+++ src/CardCollection.java
@@ -0,0 +1,10 @@
+
+public class CardCollection {
+	public static String[] cards = {"c2","c3","c4","c5","c6","c7","c8","c8","c9","c10","ca","cj","ck","cq",
+									"d2","d3","d4","d5","d6","d7","d8","d8","d9","d10","da","dj","dk","dq",
+									"h2","h3","h4","h5","h6","h7","h8","h8","h9","h10","ha","hj","hk","hq",
+									"s2","s3","s4","s5","s6","s7","s8","s8","s9","s10","sa","sj","sk","sq"};
+	
+	
+	
+}
