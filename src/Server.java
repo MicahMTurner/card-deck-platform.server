6ac00f24305004cb3d7e580a6db3d0cb9449cbd8
Irzh <Irzh@localhost>
2012-03-03 20:53:55 +0200

    (no commit message)

--- src/Server.java
+++ src/Server.java
@@ -6,13 +6,54 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
+
+
 
 
 public class Server {
+	public static String[] cardCollection = {"c2","c3","c4","c5","c6","c7","c8","c8","c9","c10","ca","cj","ck","cq",
+											 "d2","d3","d4","d5","d6","d7","d8","d8","d9","d10","da","dj","dk","dq",
+											 "h2","h3","h4","h5","h6","h7","h8","h8","h9","h10","ha","hj","hk","hq",
+											 "s2","s3","s4","s5","s6","s7","s8","s8","s9","s10","sa","sj","sk","sq"};
+	
+	
+	
 	public static ArrayList<ConnectionObject> connectoins = new ArrayList<ConnectionObject>();
+	
+	static ArrayList<String> player1Cards = new ArrayList<String>();
+	static ArrayList<String> player2Cards = new ArrayList<String>();
+	
+	private static void onLoad(){
+		ArrayList<String> cards = new ArrayList<String>();
+		
+		for(String s : cardCollection)
+			cards.add(s);
+		
+		Random generator = new Random();
+		
+		// give cards to player 1.
+		for(int i=0 ; i<6; i++){
+			int cardIndex = generator.nextInt(cards.size());
+			player1Cards.add(cards.get(cardIndex));
+			cards.remove(cardIndex);
+		}
+		
+		
+		// give cards to player 2.
+		for(int i=0 ; i<6; i++){
+			int cardIndex = generator.nextInt(cards.size());
+			player1Cards.add(cards.get(cardIndex));
+			cards.remove(cardIndex);
+		}
+		
+	}
+	
+	
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		onLoad();
 		
 		
 		
